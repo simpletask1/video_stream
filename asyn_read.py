984f24feb38c778e225d24771c6246577b94e614
liyang <1823557372@qq.com>
2021-05-20 11:28:55 +0800

    add tail call optimization for asyn read

--- asyn_read.py
+++ asyn_read.py
@@ -9,6 +9,37 @@ from datetime import datetime
 from onceorauto.general import set_logger
 
 
+# 递归函数装饰器（用于重复读流）
+class TailRecurseException(BaseException):
+    def __init__(self, args, kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+def tail_call_optimized(g):
+    """
+    This function decorates a function with tail call
+    optimization. It does this by throwing an exception
+    if it is it's own grandparent, and catching such
+    exceptions to fake the tail call optimization.
+
+    This function fails if the decorated5
+    function recurses in a non-tail context.
+    """
+    def func(*args, **kwargs):
+        f = sys._getframe()
+        if f.f_back and f.f_back.f_back and f.f_back.f_back.f_code == f.f_code:
+            raise TailRecurseException(args, kwargs)
+        else:
+            while 1:
+                try:
+                    return g(*args, **kwargs)
+                except TailRecurseException as e:
+                    args = e.args
+                    kwargs = e.kwargs
+    func.__doc__ = g.__doc__
+    return func
+
+
 class readCamera(threading.Thread):
     def __init__(self, id, logger, rtsp_address):
         threading.Thread.__init__(self)
@@ -21,7 +52,7 @@ class readCamera(threading.Thread):
         self.exception = None
         self.exc_traceback = ''
 
-    # @tail_call_optimized
+    @tail_call_optimized
     def read_vid(self):
         vid = cv2.VideoCapture(self.rtsp)
         self.logger.info('c{}_isOpened {}'.format(self.id, vid.isOpened()))
